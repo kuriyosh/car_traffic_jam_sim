ab27981f839d6f4766ca5de61d5499d6b6f5e738
torith <yosyos0306@gmail.com>
2017-12-06 19:24:40 +0900

    トラフィックカウンター配置未完成

--- sub/tools/plot_highway.py
+++ sub/tools/plot_highway.py
@@ -39,24 +39,39 @@ def convertXY2LonLat(x,y):
     return proj(x, y, inverse=True)
 
 def distance2D(lon1,lat1,lon2,lat2):
-    result = proj.inv(lon1,lat1,lon2,lat2)
-    return result[2]
+    g = pyproj.Geod(ellps='WGS84')
+    result = g.inv(lon1,lat1,lon2,lat2)
+    return result[2]/1000
 
 if __name__ == '__main__' :
+    print(distance2D(139.500319,35.485370,139.994436,35.915889))
     highway_tree = ET.parse('../data/highway.xml')
     highway_root = highway_tree.getroot()
+    distance = 0
     for child in highway_root:
         if 'shape' in child.attrib:
             PAST_POS=[0,0]
+            past_pos=[0,0]
             for pos_pair in child.attrib['shape'].split():
                 (x,y) = pos_pair.split(',')
-                (lon,lat)=convertXY2LonLat(float(x),float(y))
+                (lon,lat) = convertXY2LonLat(float(x),float(y))
                 coodinate = [int(abs(lat-origin[1])/height * GRAPH_SIZE),
-                                 int(abs(lon-origin[0])/width * GRAPH_SIZE)]
+                             int(abs(lon-origin[0])/width * GRAPH_SIZE)]
                 if PAST_POS!=[0,0]:
+                    line = distance2D(lon,lat,past_pos[0],past_pos[1])
                     cv2.line(img, (PAST_POS[0], PAST_POS[1]),
                              (coodinate[0],coodinate[1]), (255, 0, 0), 3)
+                    if distance > 2 :
+                        traffic_counter_lat = (lat - past_pos[1]) * (distance - 2)/line + past_pos[1]
+                        traffic_counter_lon = (lon - past_pos[0]) * (distance - 2)/line + past_pos[0]
+                        cv2.circle(img,
+                                   (int(abs(traffic_counter_lat-origin[1])/height * GRAPH_SIZE)
+                                    ,int(abs(traffic_counter_lon-origin[0])/width * GRAPH_SIZE))
+                                   ,10,(0,0,255),-1)
+                        distance = 0
+                    distance += line
                 PAST_POS = coodinate
+                past_pos = (lon,lat)
     cv2.imwrite('img.jpg',img)
 
                 
